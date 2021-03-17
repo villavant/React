022e2097d6bcac50dc53d41858aec280f796b29f
villavant <jorge.villavicencio10@gmail.com>
2021-03-17 00:34:17 -0500

    learning conditional rendering

--- react-advanced-2021/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ react-advanced-2021/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,28 +1,41 @@
-import React, {
-  useState,
-  useEffect
-} from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import React, { useState, useEffect } from "react";
+const url = "https://api.github.com/users/QuincyLarson";
 const MultipleReturns = () => {
   const [isloading, setIsLoading] = useState(false);
-  const [isError,setError] = useState(false);
-  const [user,setUser] = useState('default user');
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState("default user");
 
-useEffect(() =>{
-  fetch(url)
-  .then((res)=> res.json())
-  .catch((error) => console.log(error));
-},[]);
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => {
+        if(resp.status >= 200 && resp.status<=299){
+          return resp.json();
+        }else{
+          setIsError(true);
+        }
+        
+      })
+      .then((user) => {
+        console.log(user);
+        const {login} = user;
+        setUser(login);
+        setIsLoading(false);
+      })
+      .catch((error) => setIsError(true));
+  }, []);
 
-  if(isloading){
-    return <h2>Loading....</h2>
+  if (isloading) {
+    return <h2>Loading....</h2>;
   }
-  if(isError){
-return (<div><h1>Error...</h1></div>)
+  if (isError) {
+    return (
+      <div>
+        <h1>Error...</h1>
+      </div>
+    );
   }
 
   return <h2> {user}</h2>;
-
 };
 
 export default MultipleReturns;
--- react-advanced-2021/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ react-advanced-2021/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
+  const [text, setText] = useState("");
+  const [isError, setIsError] = useState(false);
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      {/* <h1>{firstValue}</h1>
+    <h1>value : {secondValue}</h1> */}
+      <h1>{text || "Jorge Villavicencio"}</h1>
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        toogle error
+      </button>
+      <h1>{isError && "Error ...."}</h1>
+      {isError ? (
+        <p>there is an error</p>
+      ) : (
+        <div>
+          <h2>There is no error</h2>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default ShortCircuit;
